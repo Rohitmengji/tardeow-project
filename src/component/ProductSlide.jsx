c4ca35e1939393b0f4f3f5a968dd78d2081268f7
Saurabh Sen <saurabh.sen.cs19@ggits.net>
2023-06-17 08:52:21 +0530

    fix: circle icons

--- src/component/ProductSlide.jsx
+++ src/component/ProductSlide.jsx
@@ -5,7 +5,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar, faSearch, faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 import "../styles/styles.css";
 
@@ -14,9 +14,9 @@ import { Autoplay, Keyboard, Navigation, Mousewheel } from "swiper";
 import { useState } from "react";
 
 function ProductSlide() {
-  const search = "../assets/Icon ionic-ios-search-1.png";
-  const heartIcon = "../assets/Group 27.png";
-  const cartIcon = "../assets/Icon feather-shopping-cart-1.png";
+  // const search = "../assets/Icon ionic-ios-search-1.png";
+  // const heartIcon = "../assets/Group 27.png";
+  // const cartIcon = "../assets/Icon feather-shopping-cart-1.png";
   const [hoverIndex, setHoverIndex] = useState(null);
 
   const handleMouseEnter = (index) => {
@@ -24,7 +24,7 @@ function ProductSlide() {
   };
 
   const handleMouseLeave = () => {
-    setHoverIndex(null);
+    // setHoverIndex(null);
   };
   const products = [
     {
@@ -120,7 +120,7 @@ function ProductSlide() {
                 border='0'
                 style={{ boxShadow: "revert-layer" }}
               >
-                <Card.Body className='d-flex flex-column justify-content-between'>
+                <Card.Body className='d-flex flex-column justify-content-between relative'>
                   <div className='content-overlay'></div>
                   <div className='text-center'>
                     <Card.Img
@@ -135,14 +135,31 @@ function ProductSlide() {
                     />
                     <Card.Title>{product.name}</Card.Title>
                     <Card.Text>{renderStars(product.rating)}</Card.Text>
-                    <Card.Text style={{marginBottom : '9px'}}>Price: {product.price}</Card.Text>
+                    <Card.Text style={{ marginBottom: '9px' }}>Price: {product.price}</Card.Text>
                   </div>
 
                   {hoverIndex === index && (
-                    <div className='middle'>
-                      <img src={search} className='icon' />
+                    <div className='middle flex gap-2'>
+                      {/* create circular icons */}
+                      <span className="bg-white px-2 py-2 text-black" style={{
+                        borderRadius: '50px',
+                      }}>
+                        <FontAwesomeIcon style={{ color: "black ", fill:"none" }} icon={faSearch} />
+                      </span>
+                      <span className="bg-white px-2 py-2 text-black" style={{
+                        borderRadius: '50px',
+                      }}>
+                         <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                          <path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"/></svg>
+                      </span>
+                      <span className="bg-white px-2 py-2 text-black" style={{
+                        borderRadius: '50px',
+                      }}>
+                        <FontAwesomeIcon style={{ color: "black", fill:"none" }} icon={faCartShopping} />
+                      </span>
+                      {/* <img src={search} className='icon' />
                       <img src={heartIcon} className='icon' />
-                      <img src={cartIcon} className='icon' />
+                      <img src={cartIcon} className='icon' /> */}
                     </div>
                   )}
                 </Card.Body>
